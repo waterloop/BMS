5bbc65212ad84e7ef7ca71159efeceb3f4865707
Benjamin-Hu <bj2hu@uwaterloo.ca>
2020-09-19 15:12:49 -0400

    L476G Test Code
    - Added Arduino emulator file
    - Added test code for STM32L476G evaluation board
    - Currently SPI communication is functioning perfectly

--- Emulator/Emulator.ino
+++ Emulator/Emulator.ino
@@ -5,6 +5,8 @@ volatile int slaveReceive, slaveSend;
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("Hello World");
   pinMode(MISO, OUTPUT);
   /* Turn on SPI in Slave Mode */
   SPCR |= _BV(SPE);
@@ -18,12 +20,14 @@ ISR (SPI_STC_vect)
 {
   /* Value received from master is stored in variable slaveReceive */
   slaveReceive = SPDR;
+  Serial.println(slaveReceive);
   received = true;
+  slaveSend = 0x003;
+  /* Send the slaveSend value to master via SPDR */
+  SPDR = slaveSend;
 }
 
 void loop()
 {
-  slaveSend = 3;
-  /* Send the slaveSend value to master via SPDR */
-  SPDR = slaveSend;
+
 }
